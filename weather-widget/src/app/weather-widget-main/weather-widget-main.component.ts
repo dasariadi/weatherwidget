57b8ccd440789eb3bdad25262dd8a04a1efaf359
Dasa Riadi <DASA.RIADI97@GMAIL.COM>
2020-11-04 11:34:19 -0500

    Final commit (will add artsy stuff later)

--- weather-widget/src/app/weather-widget-main/weather-widget-main.component.ts
+++ weather-widget/src/app/weather-widget-main/weather-widget-main.component.ts
@@ -12,13 +12,21 @@ export class WeatherWidgetMainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(){
+    this.WeatherData = {
+      main : {},
+      isDay: true
+    }
     this.getWeatherData();
     console.log(this.WeatherData);
   }
 
   getWeatherData(){
-    let data = JSON.parse('{"coord":{"lon":-79.42,"lat":43.7},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":279.03,"feels_like":275.71,"temp_min":278.71,"temp_max":279.26,"pressure":1019,"humidity":56,"sea_level":1019,"grnd_level":999},"visibility":10000,"wind":{"speed":1.47,"deg":122},"clouds":{"all":100},"dt":1604435107,"sys":{"type":1,"id":718,"country":"CA","sunrise":1604404607,"sunset":1604441138},"timezone":-18000,"id":6167865,"name":"Toronto","cod":200}');
-    this.setWeatherData(data)
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=6614716def7e71d5821946f1cae6337c')
+    .then(response => response.json())
+    .then(data=>{this.setWeatherData(data);})
+
+    // let data = JSON.parse('{"coord":{"lon":-79.42,"lat":43.7},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":279.03,"feels_like":275.71,"temp_min":278.71,"temp_max":279.26,"pressure":1019,"humidity":57,"sea_level":1019,"grnd_level":999},"visibility":10000,"wind":{"speed":1.47,"deg":122},"clouds":{"all":100},"dt":1604435107,"sys":{"type":1,"id":718,"country":"CA","sunrise":1604404607,"sunset":1604441138},"timezone":-18000,"id":6167865,"name":"Toronto","cod":200}');
+    // this.setWeatherData(data)
   };
 
   setWeatherData(data){
